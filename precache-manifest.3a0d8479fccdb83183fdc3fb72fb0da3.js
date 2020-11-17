9e0c5c86a3a9057a26a2ba1db9415b3460474110
Leszek Parkot <cedro1812@gmail.com>
2020-11-17 18:24:09 +0100

    Updates

---
+++ precache-manifest.3a0d8479fccdb83183fdc3fb72fb0da3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1ff9008ee2d2fca147c136793278f6",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "d9751ab4b5ca5b48d71b",
+    "url": "/calendar/static/css/2.f38eb524.chunk.css"
+  },
+  {
+    "revision": "4af992359788075cb22c",
+    "url": "/calendar/static/css/main.28f98a81.chunk.css"
+  },
+  {
+    "revision": "d9751ab4b5ca5b48d71b",
+    "url": "/calendar/static/js/2.ac4c7754.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/calendar/static/js/2.ac4c7754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af992359788075cb22c",
+    "url": "/calendar/static/js/main.85292245.chunk.js"
+  },
+  {
+    "revision": "43fa0801dad271e7e1f2",
+    "url": "/calendar/static/js/runtime-main.5f1e30e4.js"
+  }
+]);
